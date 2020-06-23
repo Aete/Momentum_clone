ed6ab0981c5569a1044051c708c3b3825d54264c
Seunggyun Han <hsg8234@naver.com>
2020-06-22 21:46:21 -0400

    create clock and form for asking for a name
    1. (new crate) index.html
    - add div and form for clock and name
    
    2. clock.js
    - print current time in js-clock div
    
    3. greeting.js
    - print user name in h4 (js-greeting greeting)
    - if localstrorage doesn't have data, display form and input for asking a name

---
+++ assets/js/clock.js
@@ -0,0 +1,17 @@
+const clockContainer = document.querySelector('.js-clock');
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const hour = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+    const minute = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+    const second = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    clockTitle.innerText = `${hour}:${minute}:${second}`;
+}
+
+function init() {
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
